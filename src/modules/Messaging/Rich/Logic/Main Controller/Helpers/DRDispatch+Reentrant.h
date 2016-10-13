edd615e92a1ce357fd65c2c515ac0cceb9b88aaf
bartekchlebek <bartek.chlebek@gmail.com>
2016-10-13 17:57:45 +0200

    Documentation added

--- src/modules/Messaging/Rich/Logic/Main Controller/Helpers/DRDispatch+Reentrant.h
+++ src/modules/Messaging/Rich/Logic/Main Controller/Helpers/DRDispatch+Reentrant.h
@@ -12,8 +12,25 @@ typedef dispatch_queue_t dispatch_recursive_queue_t;
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Creates a serial dispatch queue than can safely be called reentrantly using dispatch_sync_recursive.
+
+ @param name Unique name of a queue
+
+ @return queue
+ */
 dispatch_recursive_queue_t dispatch_queue_create_recursive_serial(const char * name);
 
+
+/**
+ Performs dispatch_sync on a queue, but allows it to be called reentrantly without deadlocking.
+ 
+ This function is intended to be used with queues created with
+ dispatch_queue_create_recursive_serial(const char * name).
+
+ @param queue queue
+ @param block block
+ */
 void dispatch_sync_recursive(dispatch_recursive_queue_t queue, dispatch_block_t block);
 
 NS_ASSUME_NONNULL_END
--- src/modules/Messaging/Rich/Logic/Main Controller/Helpers/NSManagedObjectContext+GlobalSerialPerformBlock.h
+++ src/modules/Messaging/Rich/Logic/Main Controller/Helpers/NSManagedObjectContext+GlobalSerialPerformBlock.h
@@ -11,6 +11,26 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSManagedObjectContext (GlobalSerialPerformBlock)
+
+/*!
+ Asynchronously performs the block on the context's queue.
+ It mimics the behaviour of -[NSManagedObjectContext performBlock:], but executes only one block at a time, despite
+ which context it was submitted to.
+ 
+ The reason behind this method is that, due to (supposedly) implementation detail change in Core Data, performBlock:
+ behaves slightly differently than it used to.
+ Pre-iOS 10 performBlock: seems to have executed blocks serially, despite being submitted to different queues.
+ On iOS 10 it seems that each NSManagedObjectContext has it's own queue.
+ 
+ This was never a correct assumption to make, especially when documentation states that performBlock: "asynchronously 
+ performs the block on the _context's_ queue". However, there are places in the SDK where the change of this behaviour
+ in iOS 10 caused some race condition bugs to pop up. This method is intended to act as a drop-in replacement to keep
+ the behaviour from before iOS 10.
+ 
+ @param block The block to perform.
+ 
+ @since 2.8.3.0
+ */
 - (void)dr_performBlockUsingManagedObjectContextsGlobalSerialQueue:(void (^)())block;
 @end
 
